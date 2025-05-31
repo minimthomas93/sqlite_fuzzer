105f773e3f7e7b25bd6626261f51c472953b06fd
Mini M Thomas <127118861+minimthomas93@users.noreply.github.com>
2025-05-31 13:33:24 +0200

    Add files via upload

---
+++ fuzzer.py
@@ -0,0 +1,18 @@
+from fuzzingbook.GrammarFuzzer import EvenFasterGrammarFuzzer
+import grammar
+import random
+import re
+
+class Fuzzer:
+    def __init__(self):
+        # This function must not be changed.
+        self.grammar = grammar.grammar
+        self.setup_fuzzer()
+    
+    def setup_fuzzer(self):
+        # This function may be changed.
+        self.fuzzer = EvenFasterGrammarFuzzer(self.grammar)
+
+    def fuzz_one_input(self) -> str:
+        # This function should be implemented, but the signature may not change.
+        return self.fuzzer.fuzz()
